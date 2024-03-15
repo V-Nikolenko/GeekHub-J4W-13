e241b4391de3e0de7f19c835a09fee6ba4da84e9
Vladyslav Nikolenko <vladyslav.nikolenko@pitchbook.com>
2024-03-15 19:33:43 +0200

    Implemented an ability to start application locally via docker for qa

--- Coursework/src/main/java/org/geekhub/niko/test/ApplicationStarter.java
+++ Coursework/src/main/java/org/geekhub/niko/test/ApplicationStarter.java
@@ -1,20 +1,26 @@
 package org.geekhub.niko.test;
 
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.Properties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ApplicationStarter {
 
     public static void main(String[] args) {
         SpringApplication.run(ApplicationStarter.class, args);
+    }
 
-        Properties properties = System.getProperties();
-
-        System.out.println(properties.get("spring.datasource.url"));
-        System.out.println(properties.get("spring.datasource.username"));
-        System.out.println(properties.get("spring.datasource.password"));
+    @Bean
+    public InitializingBean printProperties(@Value("${spring.datasource.url}") String url,
+                                            @Value("${spring.datasource.username}") String username,
+                                            @Value("${spring.datasource.password}") String password) {
+        return () -> {
+            System.out.println(url);
+            System.out.println(username);
+            System.out.println(password);
+        };
     }
 }
--- Dockerfile
+++ Dockerfile
@@ -3,8 +3,7 @@ WORKDIR /app-source
 COPY build.gradle settings.gradle gradlew ./
 COPY gradle ./gradle
 COPY Coursework ./Coursework
-RUN ./gradlew clean Coursework:bootJar -x Coursework:checkstyleMain
-
+RUN ./gradlew clean Coursework:bootJar
 
 FROM eclipse-temurin:17-jdk-alpine
 COPY --from=build /app-source/Coursework/build/libs/*.jar /app.jar
